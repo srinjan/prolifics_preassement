0a3f30520680b5f080548ed7470952db78e4a95e
srinjan <sinjandutta016@gmail.com>
2018-06-11 10:09:38 +0530

    Add files via upload

---
+++ Gamma.java
@@ -0,0 +1,21 @@
+class Alpha {
+ String getType() {
+ return "alpha";
+ }
+}
+class Beta extends Alpha {
+ String getType() {
+ return "beta";
+ }
+}
+public class Gamma extends Beta {
+ String getType() {
+ return "gamma";
+ }
+ public static void main(String[] args) {
+ Gamma g1 = (Gamma)new Alpha();
+ Gamma g2 = (Gamma)new Beta();
+ System.out.println(g1.getType() + " "
+ + g2.getType());
+ }
+}
